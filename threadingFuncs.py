01053b64931630c9710eece5d4a2c8f9ab770f37
mariamishinjiashvili <167375905+mariamishinjiashvili@users.noreply.github.com>
2025-12-07 16:21:51 -0500

    Update threadingFuncs.py

--- threadingFuncs.py
+++ threadingFuncs.py
@@ -4,6 +4,18 @@
 
 import threading
 import time
+from visual import Visual
+from sorting import Sorting
+import numpy as np
+from matplotlib import pyplot as plt
+import heapq
+import concurrent.futures
+import pandas as pd
+
+# Tkinter visualization of the graphs using matplotlib (visual.py)
+# Threading functions and timing (threadingFuncs.py)
+# Sorting functions: selection, merge, and xyz based on user input to compare time took to sort for n threads (sorting.py)
+
 import tkinter
 
 from visual import Visual
@@ -107,7 +119,6 @@ class Thread:
             print("Insertion Sort: ", sortedMedChargesInsertion)
         #elif functionName == 'Merge Sort':
             #print("Merge Sort: ", sortedMedChargesMerge)
-        return (end - start)
 
     def callVisualFunction(self, data, numOfThreads, functionName):
         threads = []
@@ -136,11 +147,88 @@ class Thread:
         end = time.perf_counter_ns()
         print(f"-------- {numOfThreads} Threads running {functionName} function -------")
         print("Execution Time: ", end - start)
-        return (end - start)
 
     def dataChunk(self, data, numOfThreads):
         dataChunks = np.array_split(data, numOfThreads)
         return dataChunks
+
+
+    def lineSearch(self, data, searchBy, searchInfo): # No thread use
+        foundElements = []
+        for idx, row in data.iterrows():
+            if row[searchBy] == searchInfo:
+                foundElements.append(row)
+        return pd.DataFrame(foundElements)
+
+
+    def multitLinearSearch(self, data, numOfThreads, searchBy, searchInfo): # Thread use
+        rows = len(data)
+        foundElements =[]
+        lock = threading.Lock()
+        chunks = self.dataChunk(data, numOfThreads)
+        def threadSearch(chunk):
+            for idx, row in chunk.iterrows():
+                if str(row[searchBy]) == str(searchInfo):
+                    lock.acquire()
+                    foundElements.append(row)
+                    lock.release()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=numOfThreads) as executor:
+            for chunk in chunks:
+                executor.submit(threadSearch, chunk)
+
+
+        return pd.DataFrame(foundElements)
+
+    '''
+    def binarySearch(self,data, numOfThreads, searchBy, searchInfo):
+        sortedData = thread.callSortFunction(data, numOfThreads, "Insertion Sort")
+        low = 0
+        high = len(sortedData) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if sortedData[mid] < x:
+                low = mid + 1
+            elif sortedData[mid] > x:
+                high = mid - 1
+            else:
+                return mid
+        return -1'''
+
+    def multitBinarySearch(self, data, numOfThreads, searchBy, searchInfo):
+        foundElements = []
+        lock = threading.Lock()
+
+        # Binary requires sorted data
+        sortedData = data.sort_values(by=searchBy).reset_index(drop=True)
+
+        chunks = self.dataChunk(sortedData, numOfThreads)
+
+        def threadSearch(chunk):
+            low = 0
+            high = len(chunk) - 1
+
+            while low <= high:
+                mid = (low + high) // 2
+                value = chunk.loc[mid, searchBy]
+
+                if str(value) < str(searchInfo):
+                    low = mid + 1
+                elif str(value) > str(searchInfo):
+                    high = mid - 1
+                else:
+                    # Lock before modifying shared list
+                    lock.acquire()
+                    foundElements.append(chunk.iloc[mid])
+                    lock.relase ()
+                    return  # exit thread
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=numOfThreads) as executor:
+            executor.map(threadSearch, chunks)
+
+        return pd.DataFrame(foundElements)
 
 
 """
@@ -149,3 +237,6 @@ https://stackoverflow.com/questions/54237067/how-to-make-tkinter-gui-thread-safe
 https://stackoverflow.com/questions/50525849/why-more-number-of-threads-takes-more-time-to-process
 https://www.reddit.com/r/learnpython/comments/10qzto6/how_does_tkinter_multithreading_work_and_why/
 """
+#https://www.geeksforgeeks.org/dsa/linear-search-using-multi-threading/
+#https://www.geeksforgeeks.org/python/python-program-for-binary-search/
+#https://www.geeksforgeeks.org/dsa/binary-search-using-pthread/
